c2b8d8fe38099c1ae3e07f1960c0edc3957ef1c9
Mike-Moe <https://github.com/mike-moe>
2024-01-02 21:45:02 -0800

    Initial Commit

---
+++ AirbnbApp/AirbnbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppApp.swift
+//  AirbnbApp
+//
+//  Created by Mahyar Moazezi on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
